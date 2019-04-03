53fb843c432bdf8d79bedff84664b29a7be2e20d
fehhfatf <felipeteixeira.19@hotmail.com>
2019-04-03 20:12:20 -0300

    Create MediaDeValorCobradoPeloFornecedorViewModel.cs

---
+++ src/SP.Application/ViewModels/MediaDeValorCobradoPeloFornecedorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Application.ViewModels
+{
+    public class MediaDeValorCobradoPeloFornecedorViewModel
+    {
+        public string NomeDoFornecedor { get; set; }
+        public string TipoDeServico { get; set; }
+        public decimal ValorMedio { get; set; }
+    }
+}
